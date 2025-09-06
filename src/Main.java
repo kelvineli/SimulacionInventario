7dd8a4f0a2c00cb762a515c88e93db6015d7eda2
kelvineli <kelvinelirp@gmail.com>
2025-09-05 19:22:35 -0600

    Inventarios

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,65 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.concurrent.atomic.AtomicInteger;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        AtomicInteger inventario= new AtomicInteger(10);
+        AtomicInteger entregastotales= new AtomicInteger(0);
+        AtomicInteger ventasTotales= new AtomicInteger(0);
+
+        int operacionesPorHilo= 100;
+
+
+        Runnable productor = () -> {
+            for (int i= 0; i< operacionesPorHilo; i++) {
+                inventario.incrementAndGet();
+                entregastotales.incrementAndGet();
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        };
+
+
+        Runnable consumidor= () -> {
+            for (int i= 0; i< operacionesPorHilo; i++) {
+                if (inventario.get()> 0) {
+                    inventario.decrementAndGet();
+                    ventasTotales.incrementAndGet();
+                } else {
+                    // no hay stock
+                }
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        };
+
+        Thread p1= new Thread(productor, "Productor-1");
+        Thread p2= new Thread(productor, "Productor-2");
+        Thread c1= new Thread(consumidor, "Consumidor-1");
+        Thread c2= new Thread(consumidor, "Consumidor-2");
+
+        int inventarioInicial= inventario.get();
+
+        p1.start();
+        p2.start();
+        c1.start();
+        c2.start();
+
+        p1.join();
+        p2.join();
+        c1.join();
+        c2.join();
+
+        System.out.println("Inventario inicial: "+ inventarioInicial);
+        System.out.println("Total de entregas realizadas: "+ entregastotales.get());
+        System.out.println("Total de ventas realizadas: "+ ventasTotales.get());
+        System.out.println("Inventario final: "+ inventario.get());
+        System.out.println("Comprobación (inicial+entregas–ventas): " +
+                (inventarioInicial+ entregastotales.get()- ventasTotales.get()));
     }
 }
